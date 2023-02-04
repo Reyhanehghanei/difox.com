20ffaa45110dff4b0b69d09cec2c7a67ef2ef640
Reyhanehghanei <regh9108@gmail.com>
2023-02-04 01:19:52 -0600

    kj

---
+++ my-scripts.js
@@ -0,0 +1,18 @@
+ 
+
+$(function () {
+  
+
+    $('.bxslider').bxSlider({
+        mode : 'horizontal',
+        speed : 1000,
+        startSlide : 2,
+        infiniteLoop : false,
+        hideControlOnEnd : true,
+        captions : true,
+        auto : true,
+        autoControls : true
+    });
+
+    
+})
