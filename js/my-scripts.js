cfde0b94f80015cc05eb0d61fecc09986ef61c3c
Reyhanehghanei <regh9108@gmail.com>
2023-03-09 16:37:33 -0600

    df

--- js/my-scripts.js
+++ js/my-scripts.js
@@ -4,11 +4,9 @@ $(function () {
     $('.bxslider').bxSlider({
         mode : 'horizontal',
         speed : 2000,
-        startSlide : 2,
-        infiniteLoop : false,
-        hideControlOnEnd : true,
-        auto : true,
-        autoControls : true
+        startSlide : 1,
+      
+       
     });
     
 })
